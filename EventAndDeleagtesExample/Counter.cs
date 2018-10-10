68861400174f3ecb53d6349428d1806d19abb9d7
Adrian Unterfinger <wuenci@msn.com>
2018-10-10 11:40:44 +0200

    Add project files.

---
+++ EventAndDeleagtesExample/Counter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventAndDeleagtesExample
+{
+    class Counter
+    {
+        //public event EventHandler SchwelleErreicht;
+
+        private int total;
+        readonly private int schwelle;
+        public event EventHandler SchwellwertErreicht;
+
+        public Counter(int schwelle)
+        {
+            this.schwelle = schwelle;
+        }
+
+        public void Add(int value)
+        {
+            total += value;
+            if(total >= schwelle)
+            {
+                OnSchwellwertErreicht(EventArgs.Empty);
+            }
+        }
+        
+        public void OnSchwellwertErreicht(EventArgs e)
+        {
+            EventHandler handler = SchwellwertErreicht;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+    }
+}
---
+++ EventAndDeleagtesExample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventAndDeleagtesExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter c = new Counter(new Random().Next(10));
+            c.SchwellwertErreicht += c_SchwellwertErreicht;
+
+            Console.WriteLine("press 'a' key to increase total");
+            while (Console.ReadKey(true).KeyChar == 'a')
+            {
+                Console.WriteLine("adding one");
+                c.Add(1);
+            }
+        }
+
+        private static void c_SchwellwertErreicht(object sender, EventArgs e)
+        {
+            Console.WriteLine("Die Schwelle wurde ereicht");
+            Environment.Exit(0);
+        }
+    }
+}
